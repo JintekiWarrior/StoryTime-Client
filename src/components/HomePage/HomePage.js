e492ba3c13b0fbb986bae4ac430a91b997a13c23
Moe Yassine <moeyassine1993@gmail.com>
2021-07-21 07:39:41 -0400

    nav link bug

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -15,7 +15,7 @@ const HomePage = (props) => {
       <div id="home-page-main-image"></div>
       <hr id="incline-line"></hr>
       <div className="begin-writing-container">
-        <Nav.Link className='begin-writing-link' href={props.user ? '/#content' : '/#sign-in'}>Begin Writing</Nav.Link>
+        <Nav.Link className='begin-writing-link' href={props.user ? '#content' : '#sign-in'}>Begin Writing</Nav.Link>
         <q className="home-quote">
           There is nothing to writing. All you do is sit down at a typewriter and bleed.
         </q>
@@ -23,7 +23,7 @@ const HomePage = (props) => {
       </div>
       <hr id="incline-line-2"></hr>
       <div className="begin-writing-container">
-        <Nav.Link className='begin-writing-link' href={props.user ? '/#content' : '/#sign-in'}>Show Stories</Nav.Link>
+        <Nav.Link className='begin-writing-link' href={props.user ? '#content' : '#sign-in'}>Show Stories</Nav.Link>
         <q className="home-quote">
           Dont walk behind me; I may not lead. Dont walk in front of me
           I may not follow. Just walk beside me and be my friend.
